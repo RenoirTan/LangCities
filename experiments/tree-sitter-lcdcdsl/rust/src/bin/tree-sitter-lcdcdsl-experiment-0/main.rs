c4cc3ffc4f4290ce926dfaaeb3b63d67eea1cc77
RenoirTan <renoirtan2005@gmail.com>
2026-08-04 16:11:58 +0800

    cargo: move experiments to somewhere else

--- experiments/tree-sitter-lcdcdsl/rust/src/bin/tree-sitter-lcdcdsl-experiment-0/main.rs
+++ experiments/tree-sitter-lcdcdsl/rust/src/bin/tree-sitter-lcdcdsl-experiment-0/main.rs
@@ -7,6 +7,12 @@ fn main() {
     println!("Parser loaded!");
 
     let source = "$mt.sc.ot_mt_m(/ˈlɛzwʊnɔɥɑ/) + $mt.sc.ot_mt_n(/ˈlɪŋ/)";
-    let tree: tree_sitter::Tree = parser.parse(source, None).unwrap();
-    println!("{:?}", tree);
+    let tree = parser.parse(source, None).unwrap();
+    let root_node = tree.root_node();
+    println!("{:?}", root_node);
+    let mut cursor = root_node.walk();
+    println!("{}", root_node.child_count());
+    for child_node in root_node.children(&mut cursor) {
+        println!("{:?}", child_node);
+    }
 }

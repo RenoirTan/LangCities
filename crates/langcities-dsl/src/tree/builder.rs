2e32a50ed2a0c444c57afebbafbbcc279b435b3a
RenoirTan <renoirtan2005@gmail.com>
2026-08-04 23:01:59 +0800

    cargo: tree builder

---
+++ crates/langcities-dsl/src/tree/builder.rs
@@ -0,0 +1,33 @@
+use std::collections::HashMap;
+
+use crate::{
+    node::{Node, NodeId},
+    tree::{Tree, TreeContext},
+};
+
+#[derive(Clone, Debug)]
+pub struct TreeBuilder {
+    pub next_node_id: NodeId,
+    pub tree: Tree,
+}
+
+impl TreeBuilder {
+    pub fn new(source: impl Into<String>) -> Self {
+        let context = TreeContext::new(source);
+        let tree = Tree::new(context, HashMap::new());
+        Self {
+            next_node_id: 0,
+            tree,
+        }
+    }
+
+    pub fn get_next_node_id(&mut self) -> NodeId {
+        let id = self.next_node_id;
+        self.next_node_id += 1;
+        id
+    }
+
+    pub fn register_node(&mut self, node: Node) -> Option<Node> {
+        self.tree.arena.insert(node.context.node_id, node)
+    }
+}
--- crates/langcities-dsl/src/tree/mod.rs
+++ crates/langcities-dsl/src/tree/mod.rs
@@ -1,5 +1,7 @@
+pub mod builder;
 pub mod context;
 pub mod tree;
 
+pub use self::builder::*;
 pub use self::context::*;
 pub use self::tree::*;

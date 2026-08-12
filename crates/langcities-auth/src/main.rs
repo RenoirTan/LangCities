ade1aa2087e8a211d6164492c84c8e4f2e55b78f
RenoirTan <renoirtan2005@gmail.com>
2026-08-12 08:23:30 +0800

    auth: add dependencies
    auth: minimal program
    
    auth: first migration
    
    auth: use environment variable for db connection string
    
    auth: give migration features too

--- crates/langcities-auth/src/main.rs
+++ crates/langcities-auth/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
+use std::{env, time::Duration};
+
+use sea_orm::{ConnectOptions, Database};
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .with_test_writer()
+        .init();
+
+    let db_url = env::var("DATABASE_URL").unwrap();
+    println!("Connecting to database: {}", db_url);
+
+    let mut opt = ConnectOptions::new(&db_url);
+    opt.max_connections(100)
+        .min_connections(5)
+        .connect_timeout(Duration::from_secs(8))
+        .acquire_timeout(Duration::from_secs(8))
+        .idle_timeout(Duration::from_secs(8))
+        .max_lifetime(Duration::from_secs(8))
+        .sqlx_logging(false) // disable SQLx logging
+        .sqlx_logging_level(log::LevelFilter::Info);
+    // .set_schema_search_path("auth_schema"); // set default Postgres schema
+
+    let db = Database::connect(opt).await.unwrap();
 }

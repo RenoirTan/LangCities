43d7b7cee05ee471d5e90e01570c2d4357b9e942
RenoirTan <renoirtan2005@gmail.com>
2026-08-22 20:44:10 +0800

    auth: extractor pattern

--- crates/langcities-auth/src/route/v1/login.rs
+++ crates/langcities-auth/src/route/v1/login.rs
@@ -1,11 +1,10 @@
 use axum::{Json, extract::State};
-use tower_sessions::Session;
 
 use crate::{
     dto::login::{PasswordLoginDto, PasswordLoginResponseDto},
     entity::prelude::*,
     error::{AuthAppError, AuthAppErrorResponseDto, AuthAppErrorTrait},
-    session::SessionWrapper,
+    session::SessionUserWrapper,
     state::AppState,
 };
 
@@ -13,14 +12,13 @@ use crate::{
 #[axum::debug_handler]
 pub async fn password_login(
     State(state): State<AppState>,
-    session: Session,
+    mut session_user: SessionUserWrapper,
     Json(dto): Json<PasswordLoginDto>,
 ) -> Result<Json<PasswordLoginResponseDto>, AuthAppErrorResponseDto> {
     let user = Users::find_by_username(&dto.username)
         .one(&state.db)
         .await
         .map_err(|e| AuthAppError::database(Some(e.into())).to_response())?;
-    let session = SessionWrapper::new(session);
     if let Some(user) = user {
         if let Some(hashed) = user.password_hash {
             let ok = state
@@ -29,10 +27,11 @@ pub async fn password_login(
                 .await
                 .map_err(|e| e.to_response())?;
             if ok {
-                session
-                    .insert("user_id", user.id)
+                session_user.get_mut_user().id = Some(user.id);
+                session_user
+                    .update_user_session()
                     .await
-                    .map_err(|e| AuthAppError::failed_session(Some(e.into())).to_response())?;
+                    .map_err(|e| e.to_response())?;
                 return Ok(Json(PasswordLoginResponseDto {
                     message: "ok".into(),
                 }));
--- crates/langcities-auth/src/session/session.rs
+++ crates/langcities-auth/src/session/session.rs
@@ -1,53 +1,78 @@
-use std::ops::{Deref, DerefMut};
-
+use axum::extract::FromRequestParts;
+use serde::{Deserialize, Serialize};
 use tower_sessions::Session;
 
+use crate::error::{AuthAppError, AuthAppErrorResponseDto, AuthAppErrorTrait};
+
 #[derive(Clone, Debug)]
-pub struct SessionWrapper {
-    inner: Session,
+pub struct SessionUserWrapper {
+    session: Session,
+    user: SessionUserDto,
 }
 
-impl SessionWrapper {
-    pub const USER_ID_KEY: &'static str = "user_id";
+impl SessionUserWrapper {
+    pub const USER_KEY: &'static str = "user";
 
-    pub fn new(session: impl Into<Session>) -> Self {
-        let session = session.into();
-        Self { inner: session }
+    pub fn new<S, U>(session: S, user: U) -> Self
+    where
+        S: Into<Session>,
+        U: Into<SessionUserDto>,
+    {
+        let (session, user) = (session.into(), user.into());
+        Self { session, user }
     }
 
-    pub async fn get_user_id(&self) -> Option<i64> {
-        self.inner.get(Self::USER_ID_KEY).await.ok().flatten()
+    pub fn get_user(&self) -> &SessionUserDto {
+        &self.user
     }
-}
 
-impl Deref for SessionWrapper {
-    type Target = Session;
-
-    fn deref(&self) -> &Self::Target {
-        &self.inner
+    pub fn get_mut_user(&mut self) -> &mut SessionUserDto {
+        &mut self.user
     }
-}
 
-impl DerefMut for SessionWrapper {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.inner
-    }
-}
-
-impl AsRef<Session> for SessionWrapper {
-    fn as_ref(&self) -> &Session {
-        &*self
+    pub async fn update_user_session(&self) -> Result<(), AuthAppError> {
+        self.session
+            .insert(Self::USER_KEY, self.user.clone())
+            .await
+            .map_err(|e| AuthAppError::failed_session(Some(e.into())))
     }
 }
 
-impl AsMut<Session> for SessionWrapper {
-    fn as_mut(&mut self) -> &mut Session {
-        &mut *self
+impl AsRef<SessionUserDto> for SessionUserWrapper {
+    fn as_ref(&self) -> &SessionUserDto {
+        self.get_user()
     }
 }
 
-impl From<Session> for SessionWrapper {
-    fn from(value: Session) -> Self {
-        Self::new(value)
+impl AsMut<SessionUserDto> for SessionUserWrapper {
+    fn as_mut(&mut self) -> &mut SessionUserDto {
+        self.get_mut_user()
+    }
+}
+
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+pub struct SessionUserDto {
+    pub id: Option<i64>,
+}
+
+impl<S> FromRequestParts<S> for SessionUserWrapper
+where
+    S: Send + Sync,
+{
+    type Rejection = AuthAppErrorResponseDto;
+
+    async fn from_request_parts(
+        parts: &mut axum::http::request::Parts,
+        state: &S,
+    ) -> Result<Self, Self::Rejection> {
+        let session = Session::from_request_parts(parts, state)
+            .await
+            .map_err(|(_s, c)| AuthAppError::failed_session(Some(c.into())).to_response())?;
+        let dto: SessionUserDto = session
+            .get(Self::USER_KEY)
+            .await
+            .map_err(|e| AuthAppError::failed_session(Some(e.into())).to_response())?
+            .unwrap_or_default();
+        Ok(SessionUserWrapper::new(session, dto))
     }
 }

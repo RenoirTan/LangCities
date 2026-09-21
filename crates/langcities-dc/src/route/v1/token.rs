0428068e9699935f2dbdbfc52828457151338e29
RenoirTan <renoirtan2005@gmail.com>
2026-09-21 23:28:44 +0800

    dc: make /v1/token/validate output something more useful

--- crates/langcities-dc/src/route/v1/token.rs
+++ crates/langcities-dc/src/route/v1/token.rs
@@ -1,16 +1,15 @@
 use crate::state::AppState;
 use axum::{Json, Router, extract::State, response::IntoResponse, routing::get};
 use langcities_jwt::payload::Claims;
-use serde_json::json;
 
 #[utoipa::path(
     get,
     path = "/v1/token/validate",
-    description = "validate access token, must be placed in bearer header"
+    description = "validate access token, must be placed in bearer header, do not rely on the response body for anything, the only thing stable is the http status"
 )]
 #[axum::debug_handler]
-pub async fn validate_token(_claims: Claims, State(_state): State<AppState>) -> impl IntoResponse {
-    Json(json!({ "valid": true }))
+pub async fn validate_token(claims: Claims, State(_state): State<AppState>) -> impl IntoResponse {
+    Json(claims)
 }
 
 pub fn get_v1_token_router() -> Router<AppState> {
